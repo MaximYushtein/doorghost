b8418be88c2a552b9cd38a9f55a0ec4779751c92
MaximYushtein <147516231+MaximYushtein@users.noreply.github.com>
2025-11-10 15:17:03 +0200

    no la polizia

---
+++ script.js
@@ -0,0 +1,63 @@
+let doors = document.getElementsByClassName("door")
+let level = document.getElementsByClassName("levelup")[0]
+let win = 1
+let again = document.getElementById("again")
+let randomNumber=Math.floor(Math.random() * 3 )
+for (let i = 0; i < 3; i = i + 1) {
+    doors[i].onclick = function (event) {
+        console.log(i);
+        for (let j = 0; j < 3; j = j + 1) {
+            // pointer-events: none блокирует двери, чтобы игрок не мог открывать больше одной двер
+            doors[j].style.pointerEvents = ("none")
+        }
+        if (randomNumber == i) {
+            doors[i].src = "door" + 2 + ".png"
+        }
+        else {
+            randomNumber=Math.floor(Math.random() * 3 )
+            doors[i].src = "door" + 1 + ".png"
+            setTimeout(() => {
+                highlight()
+                for (let m = 0; m < 3; m = m + 1) {
+                    // pointer-events: auto разблокирует двери, так, что игрок сможет снова на них кликать
+                    doors[m].style.pointerEvents = ("auto")
+                    doors[m].src = "door0.png"
+
+                     
+                }
+                win = win + 1
+                level.innerHTML = "level:" + win
+            }, 1000);
+
+        }
+
+    }
+
+}
+again.onclick = function (event) {
+    for (let k = 0; k < 3; k = k + 1) {
+        doors[k].style.pointerEvents = ("auto")
+        doors[k].src = "door0.png"
+        
+    }
+    win=1
+     level.innerHTML = "level:" + win 
+    console.log("try again");
+   
+}
+
+function highlight(){
+    setTimeout(() => {
+        
+      let door=doors[randomNumber]
+      door.classList.add("light")
+      
+        setTimeout(() => {
+            doors.classList.remove("light")
+        }, 500)                        
+    },Math.floor(Math.random() * 20000 ))
+    
+}
+highlight()
+
+// Если нажать на try again, то уровень обнуляется
